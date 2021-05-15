aa538d1e0ffa6a839ab887f059aa6b8509772ca3
Mike Brasher <33470429+mikebrasher@users.noreply.github.com>
2021-05-15 16:00:45 -0500

    Update Assassination
    Updated priority to roughly match icy veins guide.  implemented garrote debuff that tracks empowered application from subterfuge talent.

--- assassination.lua
+++ assassination.lua
@@ -29,6 +29,7 @@ local buff_enum =
       -- buffs
       ENVENOM = 32645,
       SLICE_AND_DICE = 315496,
+      SUBTERFUGE = 115192,
       
       -- debuffs
       GARROTE = 703,
@@ -43,7 +44,7 @@ local buff_enum =
 ----------------- talents ------------------
 local talents = prototype.talentlist:new(
    {
-      --absolutecorruption = prototype.talent:new(2, 2),
+      subterfuge = prototype.talent:new(2, 2),
    }
 )
 
@@ -51,6 +52,7 @@ local talents = prototype.talentlist:new(
 ----------------- skills ------------------
 local skills = prototype.datalist:new(
    {
+      ambush       = prototype.skill:new(rogue.skill_enum.AMBUSH),
       garrote      = prototype.skill:new(skill_enum.GARROTE),
       mutilate     = prototype.skill:new(skill_enum.MUTILATE),
       envenom      = prototype.skill:new(skill_enum.ENVENOM),
@@ -67,9 +69,84 @@ local skills = prototype.datalist:new(
 local player_buffs = prototype.datalist:new(
    {
       envenom      = prototype.buff:new("player", buff_enum.ENVENOM),
-      sliceanddice = prototype.buff:new("player", buff_enum.SLICE_AND_DICE),
+      sliceanddice = prototype.pandemicbuff:new("player", buff_enum.SLICE_AND_DICE, rogue.skill_enum.SLICE_AND_DICE),
+      subterfuge   = prototype.buff:new("player", buff_enum.SUBTERFUGE),
    }
 )
+
+
+----------------- garrote ------------------
+local garrote = extends(prototype.pandemicbuff)
+
+function garrote:new()
+   local o = garrote.__super.new(self, "target", buff_enum.GARROTE, skill_enum.GARROTE)
+   o.subterfuge = false
+   o.database = {}
+   setmetatable(o, self)
+   return o
+end
+
+function garrote:clear(guid)
+   self.database[guid] = nil
+end
+    
+function garrote:record(guid, timestamp)
+
+   --print(string.format("garrote: %f", timestamp))
+   self.database[guid] = self.database[guid] or {}
+   
+   local entry = self.database[guid]
+   entry.subterfuge = IsStealthed() or player_buffs.subterfuge.active
+   entry.timestamp = timestamp
+   
+end
+    
+function garrote:update(now)
+        
+   garrote.__super.update(self, now)
+
+   if (not talents.subterfuge.selected) then
+      return
+   end
+   
+   if (UnitExists("target")) then
+      
+      local targetGUID = UnitGUID("target")
+      local entry = self.database[targetGUID]
+      if (entry) then
+	 self.subterfuge = entry.subterfuge
+      end
+      
+   end
+
+   self.subterfuge = self.subterfuge and self.active
+   
+end
+
+function garrote:cleu(event, timestamp, subevent, _, sourceGUID, sourceName, _, _, destGUID, destName, _, _, spellID, spellName)
+   
+   if ((sourceGUID == common.player.guid) and
+      (spellID == buff_enum.GARROTE)) then
+      
+      if (subevent == "SPELL_AURA_APPLIED") then
+	 --print("garrote applied")
+	 self:record(destGUID, timestamp)
+      elseif (subevent == "SPELL_AURA_REFRESH") then
+	 --print("garrote refresh")
+	 self:record(destGUID, timestamp)
+      elseif (subevent == "SPELL_AURA_REMOVED") then
+	 --print("garrote removed")
+	 self:clear(destGUID)
+      end
+      
+   elseif (subevent == "UNIT_DIED") then
+      
+      --print("garrote unit died")
+      self:clear(destGUID)
+      
+   end
+   
+end
 
 
 ----------------- target debuffs ------------------
@@ -78,13 +155,13 @@ local target_debuffs = prototype.datalist:new(
       -- normal
       --haunt                = prototype.buff:new("target", buff_enum.HAUNT),
       -- pandemic
-      garrote = prototype.pandemicbuff:new("target", buff_enum.GARROTE, skill_enum.GARROTE),
+      garrote = garrote:new(),
       rupture = prototype.pandemicbuff:new("target", buff_enum.RUPTURE, skill_enum.RUPTURE),
    }
 )
 
 
------------------ affliction ------------------
+----------------- assassination ------------------
 local assassination = extends(prototype.spec)
 
 function assassination:new()
@@ -106,7 +183,11 @@ function assassination:new()
 end
 
 function assassination:load()
-   assassination.__super.load(self)   
+   
+   assassination.__super.load(self)
+   
+   self:register(self.target_debuffs.garrote, "COMBAT_LOG_EVENT_UNFILTERED", self.target_debuffs.garrote.cleu)
+   
 end
 
 function assassination:update(now)
@@ -119,49 +200,59 @@ function assassination:update(now)
    local skill = skill_enum.NIL
    
    if (InCombatLockdown()) then
-      if (skills.garrote.usable and
+      if (skills.sliceanddice.usable and
 	     (
-		target_debuffs.garrote.pandemic.active and
-		   (rogue.combopoints.deficit >= 1)
+		(player_buffs.sliceanddice.pandemic.active) and
+		   (rogue.combopoints.deficit <= 1)
 	     )
       ) then
-	 skill = skill_enum.GARROET
-      elseif (skills.fanofknives.usable and
-		 (
-		    (common.enemies.melee >= 3) and
-		       (rogue.combopoints.deficit >= 1)
-		 )
-	      
-      ) then
-	 skill = skill_enum.MUTILATE
-      elseif (skills.mutilate.usable and
-		 (rogue.combopoints.deficit >= 2)
-      ) then
-	 skill = skill_enum.MUTILATE
-      elseif (skills.shiv.usable and
-		 (rogue.combopoints.deficit == 1)
-      ) then
-	 skill = rogue.skill_enum.SHIV
+	 skill = rogue.skill_enum.SLICE_AND_DICE
       elseif (skills.rupture.usable and
 		 (
 		    target_debuffs.rupture.pandemic.active and
-		       (rogue.combopoints.current >= 4)
+		       (rogue.combopoints.deficit <= 1)
 		 )
       ) then
 	 skill = skill_enum.RUPTURE
-      elseif (skills.sliceanddice.usable and
+      elseif (skills.garrote.usable and
 		 (
-		    (player_buffs.sliceanddice.remaining < 5) and
-		       (rogue.combopoints.current >= 4)
-		 )
+		    (
+		       target_debuffs.garrote.pandemic.active and
+			  (
+			     -- don't override empowered garrote with unempowered
+			     (not target_debuffs.garrote.subterfuge) or
+				-- new garrote will be empowered
+				(player_buffs.subterfuge.active)
+			  )
+		    ) and
+		       (not rogue.combopoints.capped)
+	     )
       ) then
-	 skill = rogue.skill_enum.SLICE_AND_DICE
+	 skill = skill_enum.GARROTE
+      elseif (skills.shiv.usable and
+		 (not rogue.combopoints.capped)
+      ) then
+	 skill = rogue.skill_enum.SHIV
       elseif (skills.envenom.usable and
-		 (
-		    (rogue.combopoints.current >= 4)
-		 )
+		 (rogue.combopoints.deficit <= 1)
       ) then
 	 skill = skill_enum.ENVENOM
+      elseif (skills.fanofknives.usable and
+		 (
+		    (common.enemies.melee >= 4) and
+		       (not rogue.combopoints.capped)
+		 )
+	      
+      ) then
+	 skill = skill_enum.FAN_OF_KNIVES
+      elseif (skills.ambush.usable and
+		 (not rogue.combopoints.capped)
+      ) then
+	 skill = rogue.skill_enum.AMBUSH
+      elseif (skills.mutilate.usable and
+		 (not rogue.combopoints.capped)
+      ) then
+	 skill = skill_enum.MUTILATE
       end
    else
       skill = skill_enum.NIL
